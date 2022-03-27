7c618affc169790a94ce061c2bfe00e35a00c5d0
999Aman <101383156+999Aman@users.noreply.github.com>
2022-03-27 12:57:25 +0530

    Add files via upload

---
+++ Comparison Operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  int y = 3;
+ 
+ // Operator ==	Equal to	x == y
+ printf("%d \n", x == y);
+// Operator !=	Not equal	x != y	
+printf("%d \n", x != y);
+// Operator >	Greater than	x > y	
+printf("%d \n", x > y);
+
+// Operator <	Less than	x < y	
+printf("%d \n", x < y);
+// Operator >=	Greater than or equal to	x >= y	
+printf("%d \n", x >= y);
+// Operator <=	Less than or equal to	x <= y
+printf("%d \n", x <= y);
+return 0;
+}
