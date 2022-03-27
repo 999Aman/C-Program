b3f8e049e309114156a5b0b2f8794adfc57211a4
999Aman <101383156+999Aman@users.noreply.github.com>
2022-03-27 12:53:07 +0530

    Add files via upload

---
+++ Assignment Operators.c
@@ -0,0 +1,41 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+ int x = 5;
+ // Operator =	x = 5
+  printf("%d \n", x);
+// Operator +=	x += 3
+x = x + 3;
+ printf("%d \n", x);
+// Operator -=	x -= 3
+x = x - 3;
+ printf("%d \n", x);
+// Operator *=	x *= 3
+x = x * 3;	
+ printf("%d \n", x);
+ //Operator /=	x /= 3
+ x = x / 3;	
+  printf("%d \n", x);
+  //Operator %=	x %= 3
+  x = x % 3;	
+  printf("%d \n", x);
+ //Operator &=	x &= 3
+ x = x & 3	;
+  printf("%d \n", x);
+  //Operator |= x |= 3
+  x = x | 3	;
+  printf("%d \n", x);
+  //Operator ^=	x ^= 3	
+  x = x ^ 3	;
+  printf("%d \n", x);
+  //Operator >>=	x >>= 3
+  x = x >> 3;	
+  printf("%d \n", x);
+  //Operator <<=	x <<= 3	
+  x = x << 3;
+ printf("%d \n", x);
+    
+    return 0;
+}
