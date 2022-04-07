cc075d02f259f3f449c1f27e7f4295326acd5845
999Aman <101383156+999Aman@users.noreply.github.com>
2022-04-07 17:47:35 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num1,num2,sum;
+  printf("Enter First Numbers :");
+  scanf("%d",&num1);
+   printf("\nEnter Second Numbers :");
+  scanf("%d",&num2);
+  sum=num1+num2;
+  printf("Sum of %d and %d is :%d",num1,num2,sum);
+  getch();
+}
